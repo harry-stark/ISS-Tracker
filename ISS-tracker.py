92c4014d3a3bb23b99378524dab4533fa1031416
Harry Stark <harsimrans008@gmail.com>
2020-05-13 01:55:44 +0530

    Add files via upload

---
+++ ISS-tracker.py
@@ -0,0 +1,15 @@
+
+
+import urllib.request, urllib.error, urllib.parse
+import json
+import time
+
+req = urllib.request.Request("https://api.wheretheiss.at/v1/satellites/25544")
+response = urllib.request.urlopen(req)
+
+obj = json.loads(response.read())
+z=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(obj['timestamp']))
+print("At Time ",z," the ISS Credentials are:")
+for a,b in obj.items():
+     print(a,":",b)
+
